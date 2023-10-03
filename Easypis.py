29355837bb82e70c22254abce1a49ea086f0ba3d
fixouck <110607640+fixouck@users.noreply.github.com>
2023-10-03 17:17:51 +0300

    Update Easypis.py

--- Easypis.py
+++ Easypis.py
@@ -1,16 +1,18 @@
 from .. import loader, utils
- #  __ _                      _    
-#  / _(_)                    | |   
+
+#  __ _                      _
+#  / _(_)                    | |
 # | |_ ___  _____  _   _  ___| | __
 # |  _| \ \/ / _ \| | | |/ __| |/ /
-# | | | |>  < (_) | |_| | (__|   < 
+# | | | |>  < (_) | |_| | (__|   <
 # |_| |_/_/\_\___/ \__,_|\___|_|\_\
-                                  
+
 # Licensed under the GNU GPLv3
-#meta developer: @fixouck_qq
+# meta developer: @fix_mods
 
 class EasyPisMod(loader.Module):
-    """залупа"""
+    """Модуль 🎭"""
+
     strings = {'name': 'easypis'}
 
     async def client_ready(self, client, db):
@@ -22,7 +24,7 @@ class EasyPisMod(loader.Module):
         args = utils.get_args_raw(message)
 
         if not args:
-            await message.edit("Ошибка: требуется текст после команды 'j'.")
+            await message.edit("Ошибка: требуется текст после команды **j**")
             return
 
         await message.delete()
